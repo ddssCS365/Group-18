4129b2579bc0c7a5774c8d986d7d07aab70e4265
Ayman818 <145354743+Ayman818@users.noreply.github.com>
2025-04-18 21:18:04 +0300

    Change the AdminDashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+
 import ManageAppointments from "./components/Therapist/ManageAppointments";
 import ViewPatients from "./components/Therapist/ViewPatients";
 import SearchResults from "./components/Therapist/SearchResults";
 import MessagePatient from "./components/Therapist/MessagePatient"; // ✅ New import
 import Login from "./components/login";
 import AdminDashboard from "./components/Admin/AdminDashboard";
+import ManageSecurityPage from "./components/Admin/ManageSecurityPage";
 import TherapistDashboard from "./components/Therapist/TherapistDashboard";
 import PatientDashboard from "./components/Patient/PatientDashboard";
 import "./App.css";
@@ -16,12 +18,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/manage-security" element={<ManageSecurityPage />} />
         <Route path="/therapist" element={<TherapistDashboard />} />
         <Route path="/therapist/appointments" element={<ManageAppointments />} />
         <Route path="/therapist/patients" element={<ViewPatients />} />
         <Route path="/therapist/search-results" element={<SearchResults />} />
         <Route path="/therapist/message" element={<MessagePatient />} /> {/* ✅ New route */}
         <Route path="/patient" element={<PatientDashboard />} />
+        
       </Routes>
     </div>
   );
